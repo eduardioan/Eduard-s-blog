3d74a71be4ac4618301fec378bce92dfe38ade7a
Eduard Ioan <vilaromservice@yahoo.com>
2022-11-19 09:41:33 +0200

    initializare proiect

---
+++ Blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from Blog import views
+from Blog.views import CreatePostView
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path("post/", CreatePostView.as_view(), name='post')
+]
---
+++ Blog/views.py
@@ -0,0 +1,23 @@
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic import CreateView
+
+from Blog.forms import PostForm
+from Blog.models import Post
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post.html'
+    success_url = reverse_lazy("home")
