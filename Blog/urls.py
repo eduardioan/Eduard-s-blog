44ebb53f85c63a00edf4e920d1858af467bc6970
Eduard Ioan <vilaromservice@yahoo.com>
2022-11-19 11:54:04 +0200

    solved new post pattern and dropdown button

--- Blog/urls.py
+++ Blog/urls.py
@@ -6,5 +6,5 @@ from Blog.views import CreatePostView
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path("post/", CreatePostView.as_view(), name='post')
+    path('blog/post/', CreatePostView.as_view(), name='post')
 ]
